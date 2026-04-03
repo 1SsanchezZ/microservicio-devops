3947d12375d3395b4ada54e81b32bb72d8fb223c
1SsanchezZ <zapasanz.401@gmail.com>
2026-04-03 13:51:18 -0300

    agregar endpoint /saludo

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"mensaje": "Microservicio funcionando"})
+
+@app.route('/health')
+def health():
+    return jsonify({"status": "ok"})
+
+@app.route('/saludo')
+def saludo():
+    return jsonify({"mensaje": "Hola desde el microservicio!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
